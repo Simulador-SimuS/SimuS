3b44b92eec544bae6c3b413422254c0511fd9ce3
Gabriel P. Silva <gabriel.silva@gmail.com>
2024-06-07 16:16:54 -0300

    Teste

--- src/uconsole.pas
+++ src/uconsole.pas
@@ -3,7 +3,7 @@
 //
 //    This file is part of simulator SimuS.
 //
-//    SimuS  is free software: you can redistribute it and/or modify
+//    SimuS is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
